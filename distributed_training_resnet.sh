d4ba140b2a6fd48c92d99d7505d8f8100e3859f6
jeff-at-431556631921 <jefferson@adaviv.com>
2022-07-08 09:01:19 -0500

    Add resnet training

--- distributed_training_resnet.sh
+++ distributed_training_resnet.sh
@@ -3,8 +3,4 @@ export CUDA_VISIBLE_DEVICES=0,1
 
 # Set the visible GPUs according to the `world_size` configuration parameter
 # Modify `data.in_memory` and `data.num_workers` based on your machine
-python train_imagenet.py --config-file rn50_16_epochs.yaml \
-    --data.train_dataset=datasets/imagenet/train_500_1_90.ffcv \
-    --data.val_dataset=datasets/imagenet/validation_500_1_90.ffcv \
-    --data.num_workers=12 --data.in_memory=1 \
-    --logging.folder=output/
+python train_imagenet.py --config-file rn50_16_epochs.yaml --data.train_dataset=datasets/imagenet/train_500_1_90.ffcv --data.val_dataset=datasets/imagenet/validation_500_1_90.ffcv --data.num_workers=12 --data.in_memory=1 --logging.folder=output/
