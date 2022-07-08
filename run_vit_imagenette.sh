706627e8b6fd979e6c916f735b4c9aa71db59d17
jeff-at-431556631921 <jefferson@adaviv.com>
2022-07-07 23:06:20 -0500

    Add script for running Imagenet training

---
+++ run_vit_imagenette.sh
@@ -0,0 +1,1 @@
+python train_vit_using_timm.py datasets/imagenette_ffcv/ --model vit_small_patch16_224 --sched cosine --epochs 300 --opt adamw -j 4 --warmup-lr 1e-6 --model-ema --model-ema-decay 0.99996 --amp --lr 5e-4 --weight-decay .05 --drop 0.1 --drop-path .1 -b 16 --log-wandb --experiment ffcv_test
